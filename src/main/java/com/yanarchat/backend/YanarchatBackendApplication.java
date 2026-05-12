27ec64ec095fbe31f3bf7f2bc88052659be7c04a
qkrwns1478 <qkrwns1478@gmail.com>
2026-05-12 17:11:36 +0900

    first commit

---
+++ src/main/java/com/yanarchat/backend/YanarchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yanarchat.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanarchatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanarchatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yanarchat/backend/YanarchatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanarchat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanarchatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
