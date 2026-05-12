ad83c48a1272496c53444aa7203ec68893a28399
qkrwns1478 <qkrwns1478@gmail.com>
2026-05-12 18:06:14 +0900

    feat: Auth 기능 추가

---
+++ src/main/java/com/yanarchat/backend/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.yanarchat.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
